1e4ee8f9f43515eebee35935333efd40f377be13
thien <thientang923@gmail.com>
2026-05-27 19:23:02 -0500

    CREATING SHIP CLASS

--- src/main/java/comp2450/model/Ship.java
+++ src/main/java/comp2450/model/Ship.java
@@ -1,25 +1,54 @@
 package comp2450.model;
 
+import java.util.List;
+
 public class Ship {
-    int size;
-    ArrayList<Coordinate> coordinates;
-    int health;
-    int currentHealth;
 
-    public Ship(int size, ArrayList<Coordinate> coordinates, int health) {
+    private int size;
+    private List<Coordinate> coordinates;
+    private int health;
+    private int currentHealth;
 
+    public Ship(int size, List<Coordinate> coordinates) {
+
+        Precondition.checkArgument(size >0, "size must be positive");
+        Precondition.checkNotNull(coordinates, "coordinates cannot be null");
+        Precondition.checkArgument(coordinates.size() == size, "cooridnate must match ship size");
+
+        this.size = size;
+        this.health = health;
+        this.currentHealth = size;
+        this.coordinates = coordinates;
     }
 
-    public void move() {
+    public void move(List<Coordinate> newCoordinates) {
 
+        Precondition.checkNotNull(newCoordinates);
+        Precondition.checkArgument(newCoordinates.size() == size, "new cooridnates must match ship size");
+
+        this.coordinates = newCoordinates;
     }
 
     public void takeDamage() {
+        Precondition.checkState(currentHealth > 0, "ship is already destroyed");
 
+        currentHealth--;
     }
 
     public void isSunk() {
-        
+        return currentHealth ==0;
+    }
+
+    public int getSize() {
+        return this.size;
+    }
+
+    public int getCurrentHealth() {
+        return this.currentHealth;
+    }
+
+    public List<Coordinate> getCoordinates() {
+        return this.coordinates;
     }
 
 }
