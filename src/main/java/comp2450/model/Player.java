c1bd33d6fd63420c25dda13e55fc489725dcc6f6
thien <thientang923@gmail.com>
2026-05-27 22:05:03 -0500

    creating player class

--- src/main/java/comp2450/model/Player.java
+++ src/main/java/comp2450/model/Player.java
@@ -1,29 +1,72 @@
 package comp2450.model;
 
-import java.util.ArrayList;
+import java.util.List;
 
 public class Player {
-    String name;
-    ArrayList<Ship> ships;
-    int successfulHits;
-    int missedAttacks;
-    Map map;
+    private String playerName;
+    private List<Ship> ships;
+    private int successfulHits;
+    private int missedAttacks;
+    private Map map;
 
-    public Player(String name) {
-        if (name != null && name != "") {
-            this.name = name;
-        }
+    public Player(String playerName, Map map) {
+
+        Precondition.checkNotNull(playerName,"player name cannot be null");
+        Precondition.checkArgument(!playerName.isBlank(),"player name cannot be empty");
+        Precondition.checkNotNull(map,"map can not be null");
+
+        this.playerName = playerName;
+        this.map = map;
+        this.ship = new ArrayList<>();
+        this.successfulHits = 0;
+        this.missedAttacks = 0;
     }
 
-    public void moveShip() {
+    public voi addShip(Ship ship) {
 
+        Precondition.checkNotNull(ship, "ship cannot be null");
+        ships.add(ship);
     }
 
-    public void placeShip() {
+    public void moveShip(Ship ship, List<Coordinate> newCoordinates) {
 
+        Precondition.checkNotNull(ship);
+        Precondition.checkArgument(ships.contains(ship),"player does not own the ship");
+
+        ship.move(newCoordinates);
     }
 
-    public void attack(Coordinate targetCoordinate) {
+    public void setSuccessfulHits() {
+        successfulHits++;
+    }
 
+    public void missedAttacks() {
+        missedAttacks++;
+    }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public List<Ship> getShips() {
+        return ships;
+    }
+
+    public Map getMap() {
+        return map;
+    }
+
+    public int getSuccessfulHits() {
+        return successfulHits;
+    }
+
+    public int getMissedAttacks() {
+        return missedAttacks;
+    }
+
+    public void placeShip(Ship ship) {
+
+        Precondition.checkNotNull(ship);
+        ships.add(ship);
     }
 }
