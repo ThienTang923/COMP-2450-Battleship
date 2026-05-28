27006739ced807dcb202859bbdc0340d799e2415
thien <thientang923@gmail.com>
2026-05-27 19:10:17 -0500

    Create Cell class and Coordinate class

--- src/main/java/comp2450/model/Cell.java
+++ src/main/java/comp2450/model/Cell.java
@@ -1,31 +1,49 @@
 package comp2450.model;
 
 public class Cell {
+
     private Coordinate coordinate;
     private Ship ship;
     private Status attackedStatus;
 
     public Cell(Coordinate coordinate) {
+
+        Precondition.checkNotNull(coordinate, "coordinate cannot be null");
+
         this.coordinate = coordinate;
         this.ship = null;
         this.attacked = false;
     }
 
-    public void containShip(Ship ship) {
-        if (ship != null) {
-            this.ship = ship;
-        }
+    public void placeShip(Ship ship) {
+        Precondition.checkNotNull(ship, "Ship cannot be null");
+        Precondition.checkState(this.ship == null, "Cell already has a ship");
+
+        this.ship = ship;
+    }
+    
+    public boolean containShip() {
+        return this.ship != null;
     }
 
     public void removeShip() {
+
         ship = null;
     }
 
     public void markAttacked() {
-
+        this.attack = true;
     }
 
-    public boolean containShip () {
-        return ship != null;
+    public boolean isAttacked() {
+        return this.attack;
+    }
+
+    public Ship getShip() {
+        return this.ship;
+    }
+
+    public Coordinate getCoordinate() {
+        return this.cooridnate;
     }
 }
