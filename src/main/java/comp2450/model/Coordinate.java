9a27685a6ae33f23887211c20671f5d9e1255138
thien <thientang923@gmail.com>
2026-05-27 19:12:20 -0500

    Create Cell class and Coordinate class

--- src/main/java/comp2450/model/Coordinate.java
+++ src/main/java/comp2450/model/Coordinate.java
@@ -3,4 +3,21 @@ package comp2450.model;
 public class Coordinate {
     private int x;
     private int y;
+
+    public Cooridnate(int x, int y) {
+
+        Preconditions.checkArgument(x >= 0, "x coordinate must be non negative");
+        Preconditions.checkArgument(y >= 0, "y coordinate must be non negative");
+
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return this.x;
+    }
+
+    public int getY() {
+        return this.y;
+    }
 }
