23b43b57f47e63950fa0e9c42694b7dbb7b5aa16
thien <thientang923@gmail.com>
2026-06-05 18:48:43 -0500

    adding some exception class to throw exception

--- src/main/java/comp2450/logic/Movement.java
+++ src/main/java/comp2450/logic/Movement.java
@@ -1,6 +1,8 @@
 package comp2450.logic;
 
 import com.google.common.base.Preconditions;
+import comp2450.exceptions.InvalidInputException;
+import comp2450.exceptions.InvalidMoveException;
 import comp2450.model.*;
 
 import java.util.ArrayList;
@@ -13,7 +15,7 @@ public class Movement {
         this.pathFinder = new PathFinder();
     }
 
-    private Coordinate getCoordinateAfterMove(Coordinate coordinate, String direction) {
+    private Coordinate getCoordinateAfterMove(Coordinate coordinate, String direction) throws InvalidMoveException {
         Preconditions.checkNotNull(coordinate, "coordinate cannot be null");
         Preconditions.checkNotNull(direction, "direction cannot be null");
 
@@ -30,26 +32,29 @@ public class Movement {
             return new Coordinate(x+1,y);
         }
 
-        return coordinate;
+        throw new InvalidMoveException("Invalid direction. Please enter UP, DOWN, LEFT or RIGHT.");
     }
-    public boolean moveNormalShip(Board board, Ship ship, String direction) {
+    public boolean moveNormalShip(Board board, Ship ship, String direction) throws InvalidMoveException {
 
         Preconditions.checkNotNull(board, "board cannot be null");
         Preconditions.checkNotNull(ship, "ship cannot be null");
         Preconditions.checkNotNull(direction, "direction cannot be null");
         Preconditions.checkArgument(ship.getShipType() == ShipType.NORMAL, "ship must be normal ship");
 
+        if (ship.getShipType() != ShipType.NORMAL) {
+            throw new InvalidMoveException("Only NORMAL ship can use normal movement.");
+        }
         Coordinate oldCoordinate = ship.getCoordinates().get(0);
         Coordinate newCoordinate = getCoordinateAfterMove(oldCoordinate, direction);
 
         if (!board.isInsideBoard(newCoordinate)) {
-            return false;
+            throw new InvalidMoveException("Ship cannot move outside the board. Choose another direction");
         }
 
         Cell newCell = board.getCell(newCoordinate);
 
         if (newCell.containShip()) {
-            return false;
+            throw new InvalidMoveException("Ship cannot move there because another ship already ocupies that location.");
         }
 
         ArrayList<Coordinate> newCoordinates = new ArrayList<>();
@@ -62,28 +67,35 @@ public class Movement {
         return true;
     }
 
-    public boolean moveSubmarine(Board board, Ship ship, Coordinate target) {
+    public boolean moveSubmarine(Board board, Ship ship, Coordinate target) throws InvalidMoveException {
         Preconditions.checkNotNull(board, "board cannot be null");
         Preconditions.checkNotNull(ship, "ship cannot be null");
         Preconditions.checkNotNull(target, "target cannot be null");
         Preconditions.checkArgument(ship.getShipType() == ShipType.SUBMARINE,"ship must be submarine");
 
-        Coordinate start = ship.getCoordinates().get(0);
+        if (ship.getShipType() != ShipType.SUBMARINE) {
+            throw new InvalidMoveException("Only SUBMARINE ships can use submarine movement.");
+        }
 
         if (!board.isInsideBoard(target)) {
-            return false;
+            throw new InvalidMoveException("Submarine target is outside the board. Choose a valid coordinate.");
         }
 
         Cell targetCell = board.getCell(target);
 
-        if (targetCell.containShip()) {
-            return false;
+        if (targetCell.isAttacked()) {
+            throw new InvalidMoveException("Submarine cannot move to a coordinate that was already attacked");
         }
 
+        if (targetCell.containShip()) {
+            throw new InvalidMoveException("Submarine cannot move there because another ship already occupies that location");
+        }
+
+        Coordinate start = ship.getCoordinates().get(0);
         boolean hasPath = this.pathFinder.hasSubmarine(board, start, target);
 
         if (!hasPath) {
-            return false;
+            throw new InvalidMoveException("Submarine cannot move there. The target must be connected by attacked cells.");
         }
 
         ArrayList<Coordinate> newCoordinates = new ArrayList<>();
